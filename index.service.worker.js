9bdc92f843b03128758b6d112180d8c551fb267e
arethali <arli@ucsd.edu>
2025-11-23 02:01:18 -0800

    threads

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763891920|1935530338';
+const CACHE_VERSION = '1763892031|2046742784';
 /** @type {string} */
 const CACHE_PREFIX = 'Portfolio-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
